6daf1e2df6a1b36abd20df179ea354bd1d151702
Tryzen Alpensky <80412260+Tryzen-0@users.noreply.github.com>
2022-07-06 11:11:23 +0200

    do math

--- src/c++/hello.cpp
+++ src/c++/hello.cpp
@@ -1,8 +1,10 @@
 // C++ is cool ig.
 
 #include <iostream>
+#include <math.h>
 
 int main () {
-    std:cout << "Hello World!";
+    std::cout << "Hello World!";
+    std::cout << sin(4)
     return 0;
 }
